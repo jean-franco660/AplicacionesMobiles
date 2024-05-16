63e4fcfd8f28173ee94115c03bbaabd3981abe26
jean_franco <82102346+jean-franco660@users.noreply.github.com>
2024-05-16 16:37:25 -0500

    Calculadora en flutter

--- calc_upeux/lib/main.dart
+++ calc_upeux/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:math'; // Importar para tener acceso a las funciones matemáticas
+
+import 'package:calc_upeux/comp/CalcButton.dart';
 import 'package:calc_upeux/comp/CustomAppBarX.dart';
 import 'package:calc_upeux/theme/AppTheme.dart';
 import 'package:flutter/material.dart';
@@ -5,15 +8,17 @@ import 'package:flutter/material.dart';
 void main() => runApp(CalcApp());
 
 class CalcApp extends StatefulWidget {
-  const CalcApp({super.key});
+  const CalcApp({Key? key}) : super(key: key);
+
   @override
-  CalcAppState createState() => CalcAppState();
+  _CalcAppState createState() => _CalcAppState();
 }
 
-class CalcAppState extends State<CalcApp> {
+class _CalcAppState extends State<CalcApp> {
   String valorAnt = '';
   String operador = '';
   TextEditingController _controller = new TextEditingController();
+
   void numClick(String text) {
     setState(() => _controller.text += text);
     print(_controller);
@@ -62,13 +67,64 @@ class CalcAppState extends State<CalcApp> {
           _controller.text =
               (int.parse(valorAnt) % int.parse(_controller.text)).toString();
           break;
+
+        case "=":
+          _controller.text =
+              (int.parse(valorAnt) % int.parse(_controller.text)).toString();
+          break;
       }
     });
   }
 
+  void sqrtOperacion(String text) {
+    setState(() {
+      double num = double.tryParse(_controller.text) ?? 0;
+      _controller.text = sqrt(num).toString();
+    });
+  }
+
+  void piOperacion(String text) {
+    setState(() {
+      _controller.text = pi.toString();
+    });
+  }
+
+  void pow2Operacion(String text) {
+    setState(() {
+      double num = double.tryParse(_controller.text) ?? 0;
+      _controller.text = pow(num, 2).toString();
+    });
+  }
+
+  void powOperacion(String text) {
+    setState(() {
+
+      num num1 = num.tryParse(_controller.text) ?? 0;
+
+      num num2 = num.tryParse(_controller.text) ?? 0;
+
+      _controller.text = pow(num1, num2).toString();
+    });
+  }
   @override
   Widget build(BuildContext context) {
-//Aqui codigo
+    List<List> labelList = [
+      ["AC", "C", "%", "/"],
+      ["7", "8", "9", "*"],
+      ["4", "5", "6", "-"],
+      ["1", "2", "3", "+"],
+      [".", "0", "00", "="],
+      ["√", "π", "^2", "^x"]
+    ];
+    List<List> funx = [
+      [clear, clear, opeClick, opeClick],
+      [numClick, numClick, numClick, opeClick],
+      [numClick, numClick, numClick, opeClick],
+      [numClick, numClick, numClick, opeClick],
+      [numClick, numClick, numClick, resultOperacion],
+      [sqrtOperacion, piOperacion, pow2Operacion, powOperacion]
+    ];
+
     AppTheme.colorX = Colors.blue;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -76,22 +132,36 @@ class CalcAppState extends State<CalcApp> {
       themeMode: AppTheme.useLightMode ? ThemeMode.light : ThemeMode.dark,
       theme: AppTheme.themeData,
       home: Scaffold(
-        appBar: CustomAppBar(accionx: accion as Function),
+        appBar: CustomAppBar(accionx: accion),
         body: SingleChildScrollView(
           padding: EdgeInsets.all(20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Card(
-                /*color:
-              Theme.of(context).colorScheme.surfaceVariant,*/
                 margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
                 child: TextField(
                   textAlign: TextAlign.end,
                   controller: _controller,
                 ),
               ),
-              //aqui codigo
+              SizedBox(height: 20),
+              ...List.generate(
+                labelList.length,
+                (index) => Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ...List.generate(
+                      labelList[index].length,
+                      (indexx) => CalcButton(
+                        text: labelList[index][indexx],
+                        callback: funx[index][indexx],
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
